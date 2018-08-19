f60c011f5a0d29b198600de3047da4e13c881560
Andreas Oikon <andreas12oikonomou@gmail.com>
2018-08-19 18:00:24 +0300

    CORRECT initial commit

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # FineasWebsite
-
+Fineas Angular Website
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 6.0.5.
 
 ## Development server
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { AppRoutes } from './app.routing';
+import { HomeComponent } from './components/home/home.component';
+import { CarsComponent } from './components/cars/cars.component';
+import { AboutComponent } from './components/about/about.component';
+import { TeamComponent } from './components/team/team.component';
+import { SponsorsComponent } from './components/sponsors/sponsors.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CarsComponent,
+    AboutComponent,
+    TeamComponent,
+    SponsorsComponent,
+    ContactComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(AppRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
