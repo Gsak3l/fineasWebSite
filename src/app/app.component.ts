9967af6dd3e94fe530529df1b37ae03174be486b
s4kis <gsakel5@gmail.com>
2019-05-16 21:30:34 +0300

    full hd non responsive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
+import { CarouselConfig } from 'ngx-bootstrap/carousel';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.scss'],
+  providers: [
+    { provide: CarouselConfig, useValue: { interval: 3000, noPause: false, showIndicators: true } }
+  ]
 })
 export class AppComponent {
-  navbarOpen = false;
-
-  toggleNavbar() {
-    this.navbarOpen = !this.navbarOpen;
-  }
+  title = 'fineasracingteam';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AppRoutes } from './app.routing';
-import { HomeComponent } from './components/home/home.component';
-import { CarsComponent } from './components/cars/cars.component';
-import { AboutComponent } from './components/about/about.component';
-import { TeamComponent } from './components/team/team.component';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
 import { SponsorsComponent } from './components/sponsors/sponsors.component';
-import { ContactComponent } from './components/contact/contact.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
+import { TeamComponent } from './components/team/team.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    CarsComponent,
-    AboutComponent,
-    TeamComponent,
     SponsorsComponent,
-    ContactComponent
+    HomeComponent,
+    AboutUsComponent,
+    TeamComponent,
+    ContactUsComponent,
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(AppRoutes),
+    AppRoutingModule, 
+    CarouselModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
