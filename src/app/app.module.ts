4fa8cd651617eee34cbdccf2f23500e1af565742
gsak3l <gsakel5@gmail.com>
2019-12-06 21:35:52 +0200

    official site is down, trying to upload to gh pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,12 @@ import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CarouselModule } from 'ngx-bootstrap/carousel';
 import { HomeComponent } from './components/home/home.component';
-import { FooterComponent } from './components/footer/footer.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    FooterComponent,
     NavbarComponent,
     routingComponents,
   ],
